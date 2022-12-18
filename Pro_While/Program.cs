9e8f93ad9346f0f31d1748af47d4d8b11d46be26
Vitali_Pokrovski <vitaliipokrovskii91@gmail.com>
2022-12-18 17:12:55 +0500

    цикл While

---
+++ Pro_While/Program.cs
@@ -0,0 +1,16 @@
+﻿// Хотим найти сумму чисел от 1 до 10
+
+int a = 1;
+int sum = -1;
+
+while (a <= 10) // работаем до тех пор, пока не вышли за 10
+{
+    a++;
+
+    Console.WriteLine (" a = " + a);
+    sum = sum + a;
+    Console.WriteLine ("Текущая сумма = " + sum);
+
+}
+
+Console.WriteLine ("Итоговая сумма = " + sum);
